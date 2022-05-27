095d264c7ab06e46417991b039f1371f1be44250
Wang <elkanaalifi@gmail.com>
2022-05-27 21:48:06 +0900

    imported dotenv and configured dotenv in app

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const createError = require('http-errors');
 const express = require('express');
 const logger = require('morgan');
+require('dotenv').config()
 
 const usersRouter = require('./routes/users');
 

72b6a2bb35c8ff9adf5537c2bc6ef08cfa21e890
Wang <elkanaalifi@gmail.com>
2022-12-28 11:17:09 +0900

    projectlist schema changed due to sharedb

--- models/classroom.js
+++ models/classroom.js
@@ -2,11 +2,14 @@ const mongoose = require('mongoose')
 const { sourceCodeSchema } = require('./sourcecode')
 const Schema = mongoose.Schema
 
+// Ideally the list of projects would be a subdocument
+// However, in this case we store the list project ids due to the nature of ShareDB
+
 const classroomSchema = new Schema({
     title: {
         type: String
     },
-    projectlist:[sourceCodeSchema]
+    projectlist:[String]
 },{timestamps: true})
 
 module.exports = {
